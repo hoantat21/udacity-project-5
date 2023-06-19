560dba4c69fbf58498824f8534f81a1c36d3a0c0
hoantat21 <hoantat.21@gmail.com>
2023-06-19 14:18:22 +0700

    update appId

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '3cb1zcoqlh'
+const apiId = '5ok0q6azpl'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
